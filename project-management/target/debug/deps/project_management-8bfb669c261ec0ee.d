f165efebb2aa335264e4488d2059420eb5c478b7
tinoh9 <bachhuynh959@gmail.com>
2022-12-12 10:26:29 +0700

    How to manage a project in Rust

---
+++ project-management/target/debug/deps/project_management-8bfb669c261ec0ee.d
@@ -0,0 +1,5 @@
+c:\Users\tino\Documents\GitHub\project-management-rs\project-management\target\debug\deps\project_management-8bfb669c261ec0ee.rmeta: src\main.rs
+
+c:\Users\tino\Documents\GitHub\project-management-rs\project-management\target\debug\deps\project_management-8bfb669c261ec0ee.d: src\main.rs
+
+src\main.rs:
---
+++ project-management/target/debug/deps/project_management-b0b613828a6df1f4.d
@@ -0,0 +1,5 @@
+c:\Users\tino\Documents\GitHub\project-management-rs\project-management\target\debug\deps\project_management-b0b613828a6df1f4.rmeta: src\main.rs
+
+c:\Users\tino\Documents\GitHub\project-management-rs\project-management\target\debug\deps\project_management-b0b613828a6df1f4.d: src\main.rs
+
+src\main.rs:
